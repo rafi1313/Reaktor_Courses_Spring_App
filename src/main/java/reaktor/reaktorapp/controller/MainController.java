12df561f558fe2fbc9311dae16154bba8e3de819
Majka <maja.misiewicz@gmail.com>
2018-08-13 13:24:37 +0200

    m.m.2

--- src/main/java/reaktor/reaktorapp/controller/MainController.java
+++ src/main/java/reaktor/reaktorapp/controller/MainController.java
@@ -1,6 +1,7 @@
 package reaktor.reaktorapp.controller;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -8,35 +9,65 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import reaktor.reaktorapp.model.entity.Contact;
+import reaktor.reaktorapp.model.entity.User;
+import reaktor.reaktorapp.model.form.RegisterUserForm;
 import reaktor.reaktorapp.service.AutoMailingService;
 import reaktor.reaktorapp.service.ContactService;
+import reaktor.reaktorapp.service.UserService;
 
 import javax.validation.Valid;
 
 @Controller
 public class MainController {
-
+    UserService userService;
     ContactService contactService;
     AutoMailingService autoMailingService;
 
     @GetMapping("/")
     public String home (Model model, Authentication auth){
+        if(auth != null){
+            UserDetails principal = (UserDetails) auth.getPrincipal();
+            model.addAttribute("principal", principal);
+        }
         return "homepage";
     }
 
-    @GetMapping("/logowanie")
+    @GetMapping("/login")
     public String login(Model model, Authentication auth){
         return "loginPage";
     }
 
-    @GetMapping("/contact")
-    public String contact(Model model, Authentication auth){
-        return "contactPage";
-    }
 
     @GetMapping("/register")
     public String register(Model model, Authentication auth){
+        if(auth != null){
+            UserDetails principal = (UserDetails) auth.getPrincipal();
+            model.addAttribute("principal", principal);
+        }
+        RegisterUserForm user = new RegisterUserForm();
+        model.addAttribute("registerUserForm", user);
         return "registerPage";
+    }
+
+    @PostMapping("/register")
+    public String register(Authentication auth, Model model, BindingResult bindingResult, @ModelAttribute @Valid RegisterUserForm registerUserForm){
+        if (bindingResult.hasErrors()){
+            return "registerPage";
+        }
+
+        if(auth != null){
+            UserDetails principal = (UserDetails) auth.getPrincipal();
+            model.addAttribute("principal", principal);
+        }
+        User user = userService.createUser(registerUserForm);
+        return "redirect:/";
+    }
+
+
+    @GetMapping("/contact")
+    public String contact(Model model, Authentication auth){
+            model.addAttribute("contact", new Contact());
+        return "contactPage";
     }
 
     @PostMapping("/contact")
@@ -58,7 +89,7 @@ public class MainController {
         contact.setEmail("");
         contact.setMessage("");
         info = "Wysłano wiadomość";
-        model.addAttribute("info",info );
+
         return "contactForm";
     }
     @GetMapping("/addEditionPage")
